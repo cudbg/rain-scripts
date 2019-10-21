5428a7307369efc0b14833c85054a21cc1e67fcb
Weiyuan Wu <doomsplayer@gmail.com>
2019-10-21 10:44:52 -0700

    useless commit message

--- scripts/figure_10.py
+++ scripts/figure_10.py
@@ -3,20 +3,20 @@ import pandas as pd
 
 data = []
 _data = load_csv("../data/figure_10_1.csv")
-y = np.mean(map(float, pluckone(_data, "AUCCR")))
-ymax = y + np.std(map(float, pluckone(_data, "AUCCR")))
-ymin = y - np.std(map(float, pluckone(_data, "AUCCR")))
+y = np.mean(map(float, pluckone(_data, "AUC")))
+ymax = y + np.std(map(float, pluckone(_data, "AUC")))
+ymin = y - np.std(map(float, pluckone(_data, "AUC")))
 
 _data = load_csv("../data/figure_10_2.csv")
 
 for d in _data:
   d['name'] = "Point\nComplaint"
   d['x'] = d['number_of_complaints']
-  d['y'] = d['AUCCR']
-  d['ymax'] = d['AUCCR']
-  d['ymin'] = d['AUCCR']
+  d['y'] = d['AUC']
+  d['ymax'] = d['AUC']
+  d['ymin'] = d['AUC']
   data.append(dict(
-    name="Agg\nComplaint"
+    name="Agg\nComplaint",
     x=d['x'],
     y=y,
     ymax=ymax,
@@ -29,6 +29,6 @@ p += geom_line()
 p += geom_point()
 p += axis_labels("Number of Complaints", "AUC")
 p += legend_bottom
-ggsave("[PointVSAgg]MNISTCountPointComplaint-1-7-from-LogReg-10000-0.1.png", p, width=4, height=2.5, scale=0.8)
+ggsave("../assets/[PointVSAgg]MNISTCountPointComplaint-1-7-from-LogReg-10000-0.1.png", p, width=4, height=2.5, scale=0.8)
 
 
--- scripts/figure_3.py
+++ scripts/figure_3.py
@@ -22,17 +22,17 @@ exit()
 
 
 
-for name, rate in [("a", 0.3), ("b", 0.5), ("c", 0.7)]:
-    data = load_rc_data("../data/figure_3%s.csv" % name)
-    maxk = max(pluckone(data, "k"))
-    func = "function(x)  (1+x) / %s" % maxk
-    p = ggplot(data, aes(x="k", y="recall", color="name", fill="name", shape="name"))
-    p += stat_function(fun=func, color=esc("grey"))
-    p += geom_line()
-    p += geom_point()
-    p += axis_labels("K", "Recall", ykwargs=dict(breaks=[0.25, 0.5, 0.75]))
-    p += legend_bottom
-    p += theme(**{"legend.position": [.35, .31]})
-    ggsave("../assets/[RC]DBL-positive-0.5-3-LogReg-%s.png" % rate, p, width=4, height=2.5, scale=0.8)
+# for name, rate in [("a", 0.3), ("b", 0.5), ("c", 0.7)]:
+#     data = load_rc_data("../data/figure_3%s.csv" % name)
+#     maxk = max(pluckone(data, "k"))
+#     func = "function(x)  (1+x) / %s" % maxk
+#     p = ggplot(data, aes(x="k", y="recall", color="name", fill="name", shape="name"))
+#     p += stat_function(fun=func, color=esc("grey"))
+#     p += geom_line()
+#     p += geom_point()
+#     p += axis_labels("K", "Recall", ykwargs=dict(breaks=[0.25, 0.5, 0.75]))
+#     p += legend_bottom
+#     p += theme(**{"legend.position": [.35, .31]})
+#     ggsave("../assets/[RC]DBL-positive-0.5-3-LogReg-%s.png" % rate, p, width=4, height=2.5, scale=0.8)
 
 
