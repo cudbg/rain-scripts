ff52c20f10782a5d02b22e0a0ac885ceb138a6de
Weiyuan Wu <doomsplayer@gmail.com>
2019-10-22 00:36:19 -0700

    add errorbar

--- scripts/figure_7.py
+++ scripts/figure_7.py
@@ -2,14 +2,28 @@ from util import *
 import pandas as pd
 
 #6a
+def f(_, items):
+  """
+  aggregation function
+  """
+  d = dict(items[0])
+  recalls = pluckone(items, "recall")
+  d['y'] = np.mean(recalls)
+  d['ymax'] = np.mean(recalls) + np.std(recalls)
+  d['ymin'] = np.mean(recalls) - np.std(recalls)
+  return [d]
+  
 data = load_rc_data("../data/figure_7a.csv")
+func = "function(x) x / %s" % pluckone(data, "ntruth")[0]
 
-# func = "function(x) x / %s" % _d['ntruth']
+data = split_and_run(data, ["proc", "name", "k"], f)
 
-p = ggplot(data, aes(x="k", y="recall", color="name", fill="name", shape="name"))
-#p += stat_function(fun=func, color=esc("grey"))
+
+p = ggplot(data, aes(x="k", y="y", ymin="ymin", ymax="ymax",color="name", fill="name", shape="name"))
+p += stat_function(fun=func, color=esc("grey"))
 p += geom_line()
 p += geom_point()
+p += geom_linerange()
 p += axis_labels("K", "Recall", ykwargs=dict(breaks=[0.25, .5, .75]))
 p += legend_bottom
 p += theme(**{"legend.position": [.35, .4]})
@@ -38,7 +52,9 @@ p = ggplot(data, aes(x="Corruption", y="y", ymax='ymax', ymin='ymin', group="nam
 p += geom_line(size=1)
 p += geom_linerange(size=.5)
 p += geom_point(size=3)
+p += axis_labels("Corruption", "AUC", ykwargs=dict(lim=[0,1], breaks=[0.2, .5, .75]))
 p += legend_bottom
+p += theme(**{"legend.position": esc("bottom")})
 ggsave("../assets/[AUCCR]MNISTJoinRow-1-7-LogReg-10000.png",
     p, postfix=postfix, width=6, height=3, scale=0.8)
 
